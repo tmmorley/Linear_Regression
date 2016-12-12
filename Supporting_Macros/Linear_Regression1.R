a6626d1cbd5a322938034922da3fb4c65f14ea0d
btoomey <bridget.9216@gmail.com>
2016-12-12 11:55:16 -0700

    Finish reports and change config so that the value of regularization can be easily toggled on and off for testing purposes.

--- Supporting_Macros/Linear_Regression1.R
+++ Supporting_Macros/Linear_Regression1.R
@@ -14,19 +14,19 @@
 suppressWarnings(library(AlteryxPredictive))
 config <- list(
   `graph.resolution` = dropdownInput('%Question.graph.resolution%' , '1x'),
-  `Model Name` = textInput('%Question.Model Name%'),
+  `Model Name` = textInput('%Question.Model Name%', "mtcars"),
   `Omit Constant` = checkboxInput('%Question.Omit Constant%' , FALSE),
   `Use Weights` = checkboxInput('%Question.Use Weights%' , FALSE),
   `Weight Vec` = dropdownInput('%Question.Weight Vec%'),
   `X Vars` = listInput('%Question.X Vars%', names(mtcars)[-1]),
   `Y Var` = dropdownInput('%Question.Y Var%', 'mpg'),
   regularization = checkboxInput('%Question.regularization%', FALSE),
-  alpha = numericInput('%Question.alpha%', NULL),
+  alpha = numericInput('%Question.alpha%', .5),
   lambda_1se = radioInput('%Question.lambda.1se%', TRUE),
   lambda_min = radioInput('%Question.lambda.min%', FALSE),
   standardize_pred = checkboxInput('%Question.standardize_pred%', TRUE),
   internal_cv = checkboxInput('%Question.internal_cv%', TRUE),
-  nfolds = numericInput('%Question.nfolds%', NULL),
+  nfolds = numericInput('%Question.nfolds%', 5),
   lambda_no_cv = numericInput('%Question.lambda_no_cv%', NULL),
   display_graphs = checkboxInput('%Question.display_graphs%', TRUE)
 ) 
